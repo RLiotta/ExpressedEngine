4028c21e204c7fba8875aaf02497eda09dfaae35
rplio_ <rplio_@DESKTOP-EM5QS7I>
2022-09-13 09:14:04 -0400

    Add project files.

---
+++ DemoGame.cs
@@ -0,0 +1,254 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+using ExpressedEngine.ExpressedEngine;
+using System.Windows.Forms;
+
+namespace ExpressedEngine
+{
+    class DemoGame : ExpressedEngine.ExpressedEngine
+    {
+        //FLOATY BOY GAME
+
+        //Shape2D player;
+        Sprite2D player;
+        Sprite2D enemy;
+        int coinsCollected = 0;
+
+        bool left;
+        bool right;
+        bool up;
+        bool down;
+
+        //Vector2 lastPos = Vector2.Zero();
+        //Current mappable Variables are: c coin, p player, g ground
+        private readonly string[,] Map =
+        {
+             {"g","g","g","g","g","g","g","g","g","g","g","g","g","g","g","g","g","g","g","g","g","g","g","g","g","g","g","g","g","g","g","g","g","g","g","g","g","g","g",},
+             {"g",".",".",".","g",".","c",".",".",".",".","c","g","g","g",".",".","c","g","c","c","c","g",".","g",".",".",".",".",".",".",".",".",".",".",".",".",".","g",},
+             {"g",".",".",".","g",".","g","g","g","g","g",".","g","g",".",".","g",".","g","g","g","c","g",".",".",".",".",".",".",".",".",".",".",".",".",".",".",".","g",},
+             {"g",".","d",".","g",".","g","c","c","g","g",".","g",".",".","g","g",".",".",".","g","c","g",".","g",".",".",".",".",".",".",".",".",".",".",".",".",".","g",},
+             {"g",".",".",".",".",".","g","c","c","g",".",".","g",".","g",".","g","g","g",".","g",".","g",".","g",".",".",".",".",".",".",".",".",".",".",".",".",".","g",},
+             {"g",".",".",".",".",".","g","c","c","g",".","g",".",".","g",".",".",".",".",".","g",".",".",".","g",".",".",".",".",".",".",".",".",".",".",".",".",".","g",},
+             {"g",".",".",".","g",".","g","c","g",".",".","g",".","g","g",".","g","g","g","g","g",".","g",".","g",".",".",".",".",".",".",".",".","c","c","c",".",".","g",},
+             {"g",".",".",".","g","c","c","c","g",".","g","g",".","g",".",".",".",".","g",".",".",".","g",".","g",".",".","e","e",".",".",".",".","c","d","c",".",".","g",},
+             {"g",".","p",".","c","c","c","c","c","c",".",".",".","c",".",".","c",".",".",".","c",".","c",".","c",".",".",".",".",".",".",".",".","c","d","c",".",".","g",},
+             {"g","g","g","g","g","g","g","g","g","g","g","g","g","g","g","g","g","g","g","g","g","g","g","g","g","g","g","g","g","g","g","g","g","g","g","g","g","g","g",},
+        };
+        public DemoGame() : base(new Vector2(615, 515), "Expressed Engine Demo") { }
+
+        public override void OnDraw()
+        {
+
+        }
+        //anything to by loaded into the game before the renderer starts
+        public override void OnLoad()
+        {
+            BackGroundColor = Color.Black;//this changes the background color
+            CameraZoom = new Vector2(.95f, .95f);
+            Sprite2D groundRef = new Sprite2D("Tiles/Blue tiles/tileBlue_03");                        
+            Sprite2D coinRef = new Sprite2D("Items/yellowCrystal");
+            Sprite2D deathRef = new Sprite2D("Other/blockRed");
+            Sprite2D enemyRef = new Sprite2D("Enemies/enemyWalking_1");
+
+            //player = new Shape2D(new Vector2(10, 10), new Vector2(10, 10), "Test");//this is for shape
+            //player = new Sprite2D(new Vector2(10, 10), new Vector2(36, 45), "Players/Player Grey/playerGrey_walk1","Player");
+            for (int i = 0; i < Map.GetLength(1); ++i)
+            {
+                for (int j = 0; j < Map.GetLength(0); ++j)
+                {
+                    if (Map[j, i] == "g")
+                    {
+                        new Sprite2D(new Vector2(i * 50, j * 50), new Vector2(50, 50), "Tiles/Blue tiles/tileBlue_03", "Ground").CreateStatic();
+                    }
+                    if (Map[j, i] == "c")
+                    {
+                        new Sprite2D(new Vector2(i * 50 + 15, j * 50 + 15), new Vector2(25, 25), coinRef, "Coin");
+                    }
+                    if (Map[j, i] == "p")
+                    {
+                        player = new Sprite2D(new Vector2(i * 50, j * 50), new Vector2(30, 40), "Players/Player Green/playerGreen_walk1", "Player");
+                        player.CreateDynamic();
+                    }
+                    if (Map[j, i] == "e")
+                    {
+                        enemy = new Sprite2D(new Vector2(i * 50, j * 50), new Vector2(30, 40), enemyRef, "Enemy");
+                        //new Sprite2D(new Vector2(i * 50 + 15, j * 50 + 15), new Vector2(25, 25), enemyRef, "Enemy");
+                        enemy.CreateDynamic();
+                        Log.Warning("Player should of been killed!!");
+                        //System.Windows.Forms.Application.Exit();
+                        //System.Windows.Forms.Application.ExitThread();
+                    }
+                    if (Map[j, i] == "d")
+                    {
+                        new Sprite2D(new Vector2(i * 50 + 15, j * 50 + 15), new Vector2(25, 25), deathRef ,"Death Block");                        
+                        Log.Warning("Player has touched a death block and should be killed!!");
+
+
+
+                        //System.Windows.Forms.Application.ExitThread();
+                    }
+                }
+            }
+            //player infront of tiles
+            //player = new Sprite2D(new Vector2(100, 100), new Vector2(30, 40), "Players/Player Green/playerGreen_walk1", "Player");
+            //player2 = new Sprite2D(new Vector2(100, 30), new Vector2(30, 40), "Players/Player Green/playerGreen_walk1", "Player2");
+        }
+        int times = 0;
+        public override void OnUpdate()
+        {
+            --CameraPosition.X;
+            if (player == null)
+                return;            
+            ++times;
+            player.UpdatePosition();
+            if(up)
+            {
+                //player.Position.Y -= 3f;
+                if (up is true)
+                {
+                    player.SetVelocity(new Vector2(0, -150));
+                    //player.AddImpulse(new Vector2(0,-2000), new Vector2(0,-2000));//new Vector2(0,-55));
+                    //player.AddForce(new Vector2(0, -9000), Vector2.Zero());// new Vector2(0,-55));
+                    //player.Position.Y -= 100f;
+                    Log.Warning($"Camera X up | {CameraPosition.X}");
+                    Log.Warning($"Player X up | {player.Position.X}");
+                }
+                else
+                {
+                    //player.SetVelocity(new Vector2(0, 0));
+                }
+            }
+            if(down)
+            {
+                //player.Position.Y += 3f;
+                //player.AddImpulse(new Vector2(0, -200), Vector2.Zero());
+                //player.AddForce(new Vector2(0, -150000), Vector2.Zero());
+
+                if (down is true)
+                {
+                    player.SetVelocity(new Vector2(0, 150));
+                    Log.Warning($"Camera X down | {CameraPosition.X}");
+                    Log.Warning($"Player X down | {player.Position.X}");
+                }
+                else
+                {
+                    player.SetVelocity(new Vector2(0, 0));
+                }                    
+            }
+            if (left)
+            {
+                //player.Position.X -= 3f;
+                //player.AddImpulse(new Vector2(-20000, 0), Vector2.Zero());
+                //player.AddForce(new Vector2(-200000, 0), Vector2.Zero());
+                if (left is true)
+                {
+                    player.AddForce(new Vector2(0, -9000), Vector2.Zero());// new Vector2(0,-55));
+                    player.SetVelocity(new Vector2(-150,0));
+
+
+                    Log.Warning($"Camera X left | {CameraPosition.X}");
+                    Log.Warning($"Player X left | {player.Position.X}");
+                }
+                else
+                {
+                    player.SetVelocity(new Vector2(0, 0));
+                }
+            }
+            if (right)
+            {
+                if (right is true)
+                {
+                    player.AddForce(new Vector2(0, -9000), Vector2.Zero());// new Vector2(0,-55));
+
+                    //player.AddImpulse(new Vector2(20000, 0), Vector2.Zero());
+                    player.SetVelocity(new Vector2(150, 0));
+                     
+                    if (player.Position.X > CameraPosition.X)
+                    {
+                        
+                    }
+                    else
+                    {
+                        player.Position.X = CameraPosition.X;
+                    }
+
+
+                    Log.Warning($"Camera X right | {CameraPosition.X}");
+                        Log.Warning($"Player X right | {player.Position.X}");                    
+                }
+                else
+                {
+                    player.SetVelocity(new Vector2(0, 0));
+                }
+            }
+            if (up is true && right is true)
+            {
+                //player.Position.Y -= 3f;
+                if (up is true && right is true)
+                {
+                    player.SetVelocity(new Vector2(150, 0));
+                    player.SetVelocity(new Vector2(0, -150));
+                    //player.AddImpulse(new Vector2(0,-2000), new Vector2(0,-2000));//new Vector2(0,-55));
+                    //player.AddForce(new Vector2(0, -9000), Vector2.Zero());// new Vector2(0,-55));
+                    //player.Position.Y -= 100f;
+                    Log.Warning($"Camera X up | {CameraPosition.X}");
+                    Log.Warning($"Player X up | {player.Position.X}");
+                }
+                else
+                {
+                    //player.SetVelocity(new Vector2(0, 0));
+                }
+            }
+
+            Sprite2D coin = player.IsColliding("Coin");
+            if (coin != null)
+            {
+                //destroys coin
+                coin.DestroySelf();
+                ++coinsCollected;
+                Log.Warning($"Coin collected! | {coinsCollected}");
+            }
+            //death block
+            Sprite2D deathBlock = player.IsColliding("Death Block");
+            if (deathBlock != null)
+            {
+                //kills player
+                player.DestroySelf();
+                //logs player death
+                if (true)
+                Log.Warning("Player has died!");
+            }
+            //if (player.IsColliding("Ground") != null)
+            //{
+            //    //Log.Info($"COLLIDING! {times}");
+            //    //++times;
+            //    //player.Position.X = lastPos.X;
+            //    //player.Position.Y = lastPos.Y;
+            //}
+            //else
+
+        }
+        public override void GetKeyDown(KeyEventArgs e)
+        {
+            if(e.KeyCode == Keys.W) { up = true; }
+            if (e.KeyCode == Keys.S) { down = true; }
+            if (e.KeyCode == Keys.A) { left = true; }
+            if (e.KeyCode == Keys.D) { right = true; }
+            if (true)            
+            Log.Info("Pressing keys!");
+        }
+        public override void GetKeyUp(KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.W) { up = false; }
+            if (e.KeyCode == Keys.S) { down = false; }
+            if (e.KeyCode == Keys.A) { left = false; }
+            if (e.KeyCode == Keys.D) { right = false; }
+            if(true)
+            Log.Info("Key was released!");
+        }
+    }
+}
---
+++ ExpressedEngine/ExpressedEngine.cs
@@ -0,0 +1,179 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+using System.Windows.Forms;
+using System.Threading;
+using Box2DX.Collision;
+using Box2DX.Common;
+using Box2DX.Dynamics;
+
+namespace ExpressedEngine.ExpressedEngine
+{
+
+    class Canvas : Form
+    {
+        public Canvas()
+        {
+            this.DoubleBuffered = true;
+        }
+    }
+    public abstract class ExpressedEngine
+    {
+        private Vector2 ScreenSize = new Vector2 (512,512);
+        private string Title = "New Game";
+        private Canvas Window = null;
+        private Thread GameLoopThread = null;
+
+
+
+
+        public static List<Shape2D> AllShapes = new List<Shape2D> ();
+        public static List<Sprite2D> AllSprites = new List<Sprite2D> ();
+
+
+        public System.Drawing.Color BackGroundColor = System.Drawing.Color.Beige;
+        
+        public Vector2 CameraZoom = new Vector2(1,1);
+        public Vector2 CameraPosition = Vector2.Zero();
+        public float CameraAngle = 0f;
+
+        // Define the size of the world. Simulation will still work
+        // if bodies reach the end of the world, but it will be slower.
+        AABB worldAABB = new AABB
+        {
+            UpperBound = new Vec2(2000,2000),
+            LowerBound = new Vec2(-2000, -2000)
+        };
+
+        // Define the gravity vector.
+        
+        Vec2 gravity = new Vec2(0.0f, 2000.0f);
+        public static World world = null;
+        // Construct a world object, which will hold and simulate the rigid bodies.
+
+        public ExpressedEngine(Vector2 SceenSize,string Title)
+        {
+            Log.Info("Game is Starting");
+            this.ScreenSize = SceenSize;
+            this.Title = Title;
+
+            Window = new Canvas();
+            Window.Size = new Size((int)SceenSize.X, (int)SceenSize.Y);
+            Window.Text = this.Title;
+            Window.Paint += Renderer;
+            Window.KeyDown += Window_KeyDown;
+            Window.KeyUp += Window_KeyUp;
+            Window.FormBorderStyle = FormBorderStyle.FixedToolWindow;
+            Window.FormClosing += Window_FormClosing;
+            GameLoopThread = new Thread(GameLoop);
+            GameLoopThread.Start();
+
+            world = new World(worldAABB, gravity, false);
+
+
+            Application.Run(Window);
+            
+        }
+
+        private void Window_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            GameLoopThread.Abort();
+        }
+
+        private void Window_KeyUp(object sender, KeyEventArgs e)
+        {
+            GetKeyUp(e);
+        }
+
+        private void Window_KeyDown(object sender, KeyEventArgs e)
+        {
+            GetKeyDown(e);
+        }
+
+        public static void RegisterShape(Shape2D shape)
+        {
+            AllShapes.Add(shape);
+        }
+        public static void UnRegisterShape(Shape2D shape)
+        {
+            AllShapes.Remove(shape);
+        }
+        public static void RegisterSprite(Sprite2D sprite)
+        {
+            AllSprites.Add(sprite);
+        }
+        public static void UnRegisterSprite(Sprite2D sprite)
+        {
+            AllSprites.Remove(sprite);
+        }
+        // Prepare for simulation. Typically we use a time step of 1/60 of a
+        // second (60Hz) and 10 iterations. This provides a high quality simulation
+        // in most game scenarios.
+        float timeStep = 1.0f / 60.0f;
+        int velocityIterations = 10;
+        int positionIterations = 3;
+        void GameLoop()
+        {
+
+            OnLoad();
+            while (GameLoopThread.IsAlive)
+            {
+                try
+                {
+                    OnDraw();
+                    Window.BeginInvoke((MethodInvoker)delegate { Window.Refresh(); });
+                    world.Step(timeStep, velocityIterations, positionIterations);
+                    OnUpdate();
+                    Thread.Sleep(1);
+                }
+                catch
+                {
+                    Log.Error("Window has not been found... Waiting...");
+                }
+            }
+        }
+
+        private void Renderer(object sender, PaintEventArgs e)
+        {
+
+            // Instruct the world to perform a single step of simulation. It is
+            // generally best to keep the time step and iterations fixed.
+            
+
+            Graphics g = e.Graphics;
+
+            g.Clear(BackGroundColor);
+
+            g.TranslateTransform(CameraPosition.X, CameraPosition.Y);
+            g.RotateTransform(CameraAngle);
+            g.ScaleTransform(CameraZoom.X,CameraZoom.Y);
+            try
+            {
+                foreach (Shape2D shape in AllShapes)
+                {
+                    g.FillRectangle(new SolidBrush(System.Drawing.Color.Red), shape.Position.X, shape.Position.Y, shape.Scale.X, shape.Scale.Y);
+                }
+                foreach (Sprite2D sprite in AllSprites)
+                {
+                    if (!sprite.IsReference)
+                    {
+                        g.DrawImage(sprite.Sprite, sprite.Position.X, sprite.Position.Y, sprite.Scale.X, sprite.Scale.Y);
+                    }
+                }
+            }
+            catch
+            {
+
+            }
+        }
+
+        public abstract void OnLoad();
+        public abstract void OnUpdate();
+        public abstract void OnDraw();
+        public abstract void GetKeyDown(KeyEventArgs e);
+        public abstract void GetKeyUp(KeyEventArgs e);
+    }
+}
